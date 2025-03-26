855e3458e52bc4702dd328729f5bd16d691b681d
Ozias Silitonga <oziassilitonga@gmail.com>
2025-03-26 00:51:57 +0000

    Fixed urls.py in project folder

--- scrandemonium_project/urls.py
+++ scrandemonium_project/urls.py
@@ -23,7 +23,7 @@ app_name = 'scrandemonium'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('rango/', include('rango.urls')),
+    path('scrandemonium/', include('scrandemonium.urls')),
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

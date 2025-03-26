c46e6b9a98442e7270d65222f25e5790f04bfc64
Alex Serna-Sullivan <2652932s@student.gla.ac.uk>
2025-03-26 11:29:12 +0000

    Fixed Forms, renamed views for consistency, added the final views. Deleted extra addRecipe.HTML, amended small mistake in recipes.HTML

--- scrandemonium/forms.py
+++ scrandemonium/forms.py
@@ -1,57 +1,31 @@
-from django import forms 
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from rango.models import Recipe, User, Review
+from .models import Recipe, User
 
-class AddRecipe(forms.ModelForm):
-    dishName = forms.CharField(max_length=128,
-                           help_text="Recipe name") #required
-    picture = forms.IntegerField(widget=forms.HiddenInput(), initial=0) #TODO required
-    video = forms.IntegerField(default=0) #TODO optional
-    steps = forms.SlugField(unique=True) #TODO required
-    ingredients = forms.SlugField(unique=True) #TODO required
-    tags = forms.SlugField(unique=True) #TODO optional
-    category = forms.SlugField(unique=True) #TODO required, use choices
-    servings = forms.IntegerField(default=0) #optional
+# -- ADD RECIPE --
+class AddRecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ('name',)
+        fields = ['title', 'meal_type', 'step', 'servings', 'cooking_time']
 
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-
-    class Meta: 
+# -- USER/PROFILE --
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
         model = User
-        fields = ('username', 'email', 'password',)
+        fields = ('username', 'email', 'password1', 'password2')
 
-class LoginForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,
-                            help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200,
-                         help_text="Please enter the URL of the page.")
-    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-
+class ProfileForm(forms.ModelForm):
     class Meta:
-        model = Page
-        exclude = ('category',)
+        model = User
+        fields = ('biography', 'profile_picture')
 
-    def clean(self):
-        cleaned_data = self.cleaned_data 
-        url = cleaned_data.get('url')
-        
-        if url and not url.startswith('http://'):
-            url = f'http://{url}'
-            cleaned_data['url'] = url
-        
-        return cleaned_data
+# -- LOGIN -- 
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150, label='Username')
+    password = forms.CharField(widget=forms.PasswordInput, label='Password')
 
-class ReviewForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-
-    class Meta: 
-        model = Review
-        fields = ('Rating', 'Details',)
-
-class UserProfileForm(forms.ModelForm): 
-    class Meta:
-        model = UserProfile
-        fields = ('website', 'picture',)
+# -- REVIEW --
+class ReviewForm(forms.Form):
+    comment = forms.CharField(widget=forms.Textarea, label='Your Comment')
+    rating = forms.IntegerField(min_value=1, max_value=5, label='Your Rating')

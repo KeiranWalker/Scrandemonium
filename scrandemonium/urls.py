4b1e13ec1dfdbf4d288c8afcf725cab2d6672971
Keiran Walker <2895682w@student.gla.ac.uk>
2025-03-26 10:55:31 +0000

    Title Picture and Some URLS

--- scrandemonium/urls.py
+++ scrandemonium/urls.py
@@ -5,12 +5,12 @@ app_name = 'scrandemonium'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.register, name='register'),
-    path('recipes/', views.recipes, name='recipes'),
-    path('recipes/<str:meal_type>/', views.recipes, name='recipes_by_category'),
+    #path('register/', views.register, name='register'),
+    #path('recipes/', views.recipes, name='recipes'),
+    #path('recipes/<str:meal_type>/', views.recipes, name='recipes_by_category'),
     path('recipes/<int:recipe_id>/', views.recipe_page, name='recipe_page'),
-    path('recipes/<int:recipe_id>/review/', views.review_recipe, name='review_recipe'),
-    path('login/', views.user_login, name='login'),
+    #path('recipes/<int:recipe_id>/review/', views.review_recipe, name='review_recipe'),
+    path('login/', views.login_view, name='login'),
     path('login/profile/<int:user_id>/', views.profile, name='profile'),
     path('login/addRecipe/', views.add_recipe, name='add_recipe'),
     path('about/', views.about, name='about'),

09eade88ea881fecc3db67e12b50e68b8fcff990
Ozias Silitonga <oziassilitonga@gmail.com>
2025-03-26 00:33:42 +0000

    Update urls.py

--- scrandemonium/urls.py
+++ scrandemonium/urls.py
@@ -1,13 +1,13 @@
-from djanog.urls import path
-from rango import views
+from django.urls import path
+from scrandemonium import views
 
 app_name = 'scrandemonium'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
-    path('recipes/<str:meal_type>/', views.recipe_, name='recipes_by_category'),
+    path('recipes/', views.recipes, name='recipes'),
+    path('recipes/<str:meal_type>/', views.recipes, name='recipes_by_category'),
     path('recipes/<int:recipe_id>/', views.recipe_page, name='recipe_page'),
     path('recipes/<int:recipe_id>/review/', views.review_recipe, name='review_recipe'),
     path('login/', views.user_login, name='login'),

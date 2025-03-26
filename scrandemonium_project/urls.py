092434b2bd019425fbff6a1444ab19df3986dd35
Ozias Silitonga <oziassilitonga@gmail.com>
2025-03-26 01:01:58 +0000

    fixed comment

--- scrandemonium_project/urls.py
+++ scrandemonium_project/urls.py
@@ -24,6 +24,5 @@ app_name = 'scrandemonium'
 urlpatterns = [
     path('', views.index, name='index'),
     path('scrandemonium/', include('scrandemonium.urls')),
-    # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
